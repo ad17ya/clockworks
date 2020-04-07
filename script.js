3e3a4118a91cece7172a95aed3f5f6932a476726
ad17ya <aditya1751999@gmail.com>
2020-04-07 15:33:34 +0530

    now the clock updates the entered values

--- script.js
+++ script.js
@@ -22,3 +22,44 @@ function soundAlarm() {
         setTimeout(playSound, 1200 * i);
     }
 }
+
+function updateValue(key, value) {
+    if(value < 0){
+        value = 0;
+        console.log("Positive numbers only");
+
+    }
+
+    if(key == "seconds"){
+        if(value < 0){
+            value = "0" + value;
+        }
+
+        if(value > 59){
+            value = "59";
+        }
+    }
+
+    $("#" +key).html(value || 0);
+    timerObj[key] = value;
+
+    console.log("Min", timerObj.minutes);
+    console.log("Sec", timerObj.seconds);
+}
+
+//self calling function ()indicate the calling of the functions
+(function detectChanges(key) {
+    console.log("detect changes");
+    let input = "#" + key +"-input";
+
+    $(input).change(function() {
+        updateValue(key, $(input).val());
+    });
+
+    $(input).keyup(function() {
+        updateValue(key, $(input).val());
+    });
+
+    return arguments.callee;
+
+})("minutes")("seconds");
