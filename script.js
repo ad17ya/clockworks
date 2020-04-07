4900622842b315d1f2a9d406b8eeda6ac58bfa18
ad17ya <aditya1751999@gmail.com>
2020-04-07 15:07:19 +0530

    added sound

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-z
+let timerObj = {
+    minutes: 0,
+    seconds: 0,
+    timerId: 0
+}
+
+function soundAlarm() {
+    let amount = 3;
+    let audio = new Audio("sound.mp3");
+
+
+    // audio.play();
+
+    function playSound() {
+        audio.pause();
+        audio.currentTime = 0;
+        audio.play();
+
+    }
+
+    for (let i = 0; i < amount;  i++) {
+        setTimeout(playSound, 1200 * i);
+    }
+}
